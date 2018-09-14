7a5bda07ca18b615fe672e76875772a36c5d947b
Thomas Meulendijks <Thomas@ThomasMeulendijks.xyz>
2018-09-14 20:39:55 +0200

    update keymap

--- keyboards/iris/keymaps/thomas/keymap.c
+++ keyboards/iris/keymaps/thomas/keymap.c
@@ -13,26 +13,39 @@ enum custom_keycodes {
   RAISE,
   ADJUST,
 };
+#define KC_ KC_TRNS
+#define _______ KC_TRNS
+#define KC_LOWR LOWER
+#define KC_RASE RAISE
+
+#define KC_BR_BACK LALT(KC_LEFT)           // Browser Back
+#define KC_BR_FORW LALT(KC_RIGHT)          // Browser Forward
+#define KC_BR_CT LCTL(KC_W) // Browser close
+#define KC_BR_NT LSFT(LCTL(KC_TAB)) //Browser next tab
+#define KC_BR_NT LCTL(KC_TAB) //Browser prev tab
+
+#define KC_ENTS LSFT_T(KC_ENT) // Shift (hold), Enter (tap)
+#define KC_ESCG GUIT_T(KC_ESC) // GUI (hold), Escape (tab)
+#define KC_ESCC LCTL_T(KC_ESC)    // Control (hold), Escape (tap)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_INS,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PLUS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_ESCC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,          KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,          KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                   KC_LALT,GUI_T(KC_ESC),KC_SPC,                    KC_ENT,   LOWER, RAISE
+                                   RAISE,    KC_ESCG,KC_SPC,               	 KC_BSPC, KC_ENTS, LOWER
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
-
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_HOME , KC_END , KC_PGUP,KC_PGDOWN,_______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -43,6 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
+
 
   [_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
